76c3380be18cf9787bc1cfc0e936c4ccce14f0f7
arielmusa <arielmusainfo@gmail.com>
2025-10-28 23:34:02 +0100

    feat: add HomePage component.
    the component imports useMovies and displays the Hero and List components passing them the required props

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
+import Hero from "../components/Hero";
+import List from "../components/List";
+import { useMovies } from "../contexts/MoviesContext";
+
 export default function HomePage() {
+  const { movies } = useMovies();
+  let heroMovie = movies[0];
+
   return (
     <>
-      <h1>Homepage</h1>
+      <div className="container">
+        <Hero {...heroMovie} />
+        <List movies={movies} />
+      </div>
     </>
   );
 }
