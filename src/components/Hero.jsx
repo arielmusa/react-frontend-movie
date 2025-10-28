9259e2ce90ff382782c08d7bcf40b35a5f6d1437
arielmusa <arielmusainfo@gmail.com>
2025-10-28 23:31:36 +0100

    feat: add Hero component.
    the component gets a movie as prop

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+export default function Hero(movie) {
+  const { title, description, director, release_year, genre } = movie;
+  return (
+    <>
+      <div className="hero">
+        <h2 className="movie-title">{title}</h2>
+        <p className="movie-description">{description}</p>
+        <a href="#" className="btn btn-primary">
+          Watch now
+        </a>
+        <div className="movie-info">
+          <span className="rating pe-3">{director}</span>
+          <span className="rating pe-3">{release_year}</span>
+          <span className="rating pe-3">{genre}</span>
+        </div>
+      </div>
+    </>
+  );
+}
