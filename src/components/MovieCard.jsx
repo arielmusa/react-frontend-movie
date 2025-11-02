9360a5bf6baef550425a93dbba95bd2ddd01acd4
arielmusa <arielmusainfo@gmail.com>
2025-11-02 23:18:31 +0100

    style: add some styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,13 +5,13 @@ export default function MovieCard(movie) {
   return (
     <>
       <div className="card h-100">
-        <img src="..." className="card-img-top" alt="..." />
+        {/*  <img src="..." className="card-img-top" alt="..." /> */}
         <div className="card-body d-flex flex-column justify-content-between">
           <div>
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p>{genre}</p>
-            <p>{director}</p>
+            <p className="card-text mb-0">{description}</p>
+            <span className="badge text-bg-primary">{genre}</span>
+            <p className="pt-2 fw-bold">{director}</p>
           </div>
           <div>
             <NavLink to={`/movies/${id}`} className="btn btn-primary">
