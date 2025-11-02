66d1f35f1a3d633554215ce060687142c96622c5
arielmusa <arielmusainfo@gmail.com>
2025-11-02 19:47:37 +0100

    feat: handle state isLoading in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,20 @@ import List from "../components/List";
 import { useMovies } from "../contexts/MoviesContext";
 
 export default function HomePage() {
-  const { movies } = useMovies();
+  const { movies, isLoading } = useMovies();
   let heroMovie = movies[0];
 
   return (
     <>
       <div className="container">
-        <Hero {...heroMovie} />
-        <List movies={movies} />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <Hero {...heroMovie} />
+            <List movies={movies} />
+          </>
+        )}
       </div>
     </>
   );
